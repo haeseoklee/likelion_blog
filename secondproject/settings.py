6ccc833d943c082b41d8d64f2e4acfe4cf2de78c
haeseoklee <interpret96@gmail.com>
2019-02-11 23:33:17 +0900

    add hosts

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '8k#q=d8_sk3(h-%d1*^7@w0g2k7_u&
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['likelion-heroku.herokuapp.com']
 
 
 # Application definition
